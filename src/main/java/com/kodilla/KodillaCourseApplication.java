49e3c053c51e554fa2ea01a74c25d9d8f0181874
mwydra <magdalena.wydra1012@gmail.com>
2021-10-05 15:32:39 +0200

    commit

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -1,5 +1,6 @@
 package com.kodilla;
 
+import com.kodilla.testing.accenture.Duplicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
